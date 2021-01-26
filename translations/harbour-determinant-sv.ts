f5b3ef05c505936334d476b9ba41529e44e70780
Åke Engelbrektson <eson57@users.noreply.github.com>
2021-01-26 08:50:05 +0100

    Create harbour-determinant-sv.ts
    Add Swedish translation, please.

---
+++ translations/harbour-determinant-sv.ts
@@ -0,0 +1,258 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>Det::ConnectionsManager</name>
+    <message>
+        <source>Login error: %1</source>
+        <translation>Inloggningsfel: %1</translation>
+    </message>
+</context>
+<context>
+    <name>Det::Humanize</name>
+    <message>
+        <source>today</source>
+        <translation>idag</translation>
+    </message>
+    <message>
+        <source>yesterday</source>
+        <translation>igår</translation>
+    </message>
+</context>
+<context>
+    <name>Det::NotificationsService</name>
+    <message numerus="yes">
+        <source>%1 messages</source>
+        <translation>
+            <numerusform>%1 meddelande</numerusform>
+            <numerusform>%1 meddelanden</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>Det::ThumbnailResponse</name>
+    <message>
+        <source>Media id `%1` does not have expected pattern: server/id</source>
+        <translation>Media-ID &quot;%1&quot; har inte förväntat mönster: Server/ID</translation>
+    </message>
+</context>
+<context>
+    <name>ErrorPage</name>
+    <message>
+        <source>Error</source>
+        <translation>Fel</translation>
+    </message>
+</context>
+<context>
+    <name>EventDelegate</name>
+    <message>
+        <source>New messages</source>
+        <translation>Nya meddelanden</translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <source>Matrix Login</source>
+        <translation>Matrix inloggning</translation>
+    </message>
+    <message>
+        <source>Server</source>
+        <translation>Server</translation>
+    </message>
+    <message>
+        <source>Username</source>
+        <translation>Användarnamn</translation>
+    </message>
+</context>
+<context>
+    <name>LoginProgressPage</name>
+    <message>
+        <source>Login Error</source>
+        <translation>Inloggningsfel</translation>
+    </message>
+    <message>
+        <source>Logging in …</source>
+        <translation>Loggar in ...</translation>
+    </message>
+</context>
+<context>
+    <name>MessageInput</name>
+    <message>
+        <source>Message</source>
+        <translation>Mededelande</translation>
+    </message>
+</context>
+<context>
+    <name>MessageRenderer</name>
+    <message>
+        <source>%1 cleared room main alias</source>
+        <translation>%1 tog bort rummets huvudalias</translation>
+    </message>
+    <message>
+        <source>%1 set room main alias to %2</source>
+        <translation>%1 satte rummets huvudalias till %2</translation>
+    </message>
+    <message>
+        <source>%1 cleared room name</source>
+        <translation>%1 tog bort rummets namn</translation>
+    </message>
+    <message>
+        <source>%1 set room name to %2</source>
+        <translation>%1 satte rummets namn till %2</translation>
+    </message>
+    <message>
+        <source>%1 removed topic</source>
+        <translation>%1 tog bort rubrik</translation>
+    </message>
+    <message>
+        <source>%1 set topic to %2</source>
+        <translation>%1 satte rubriken till %2</translation>
+    </message>
+    <message>
+        <source>%1 activated end-to-end encryption</source>
+        <translation>%1 aktiverade slutpunkt-till-slutpunktkryptering</translation>
+    </message>
+    <message>
+        <source>upgraded room to version %1</source>
+        <translation>uppgraderade rummet till version %1</translation>
+    </message>
+    <message>
+        <source>Unsupported event: %1</source>
+        <translation>Händelse som saknar stöd: %1</translation>
+    </message>
+    <message>
+        <source>Unsupported message</source>
+        <translation>Meddelande som saknar stöd</translation>
+    </message>
+    <message>
+        <source>%1 has joined</source>
+        <translation>%1 har anslutit</translation>
+    </message>
+    <message>
+        <source>%1 was invited</source>
+        <translation>%1 blev inbjuden</translation>
+    </message>
+    <message>
+        <source>%1 is banned</source>
+        <translation>%1 är bannlyst</translation>
+    </message>
+    <message>
+        <source>%1 has left</source>
+        <translation>%1 har lämnat</translation>
+    </message>
+    <message>
+        <source>%1 knocked</source>
+        <translation>%1 knackade</translation>
+    </message>
+    <message>
+        <source>%1 has undefined state</source>
+        <translation>%1 har odefinierad status</translation>
+    </message>
+    <message>
+        <source>%1 has changed name to %2</source>
+        <translation>%1 har bytt namn till %2</translation>
+    </message>
+    <message>
+        <source>%1 has changed avatar</source>
+        <translation>%1 har bytt avatar</translation>
+    </message>
+    <message>
+        <source>%1 upgraded room to version %2</source>
+        <translation>%1 uppgraderade rummet till version %2</translation>
+    </message>
+    <message>
+        <source>%1 created room</source>
+        <translation>%1 skapade ett rum</translation>
+    </message>
+    <message>
+        <source>🗑️ redacted message</source>
+        <translation>🗑️ redigerat meddelande</translation>
+    </message>
+    <message>
+        <source>🗑️ redacted: %2</source>
+        <translation>🗑️ redigerat: %2</translation>
+    </message>
+    <message>
+        <source>%1 set room aliases on server %2 to %3</source>
+        <translation>%1satte rumsalias på server %2 till %3</translation>
+    </message>
+    <message>
+        <source>Unnamed user</source>
+        <translation>Namnlös användare</translation>
+    </message>
+</context>
+<context>
+    <name>PublicRoomListPage</name>
+    <message>
+        <source>Public Rooms</source>
+        <translation>Offentliga rum</translation>
+    </message>
+</context>
+<context>
+    <name>RoomDetailsPage</name>
+    <message>
+        <source>Matrix Id</source>
+        <translation>Matrix-ID</translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation>Namn</translation>
+    </message>
+    <message>
+        <source>Aliases</source>
+        <translation>Alias</translation>
+    </message>
+    <message>
+        <source>Uses Encryption</source>
+        <translation>Använd kryptering</translation>
+    </message>
+    <message>
+        <source>Tags</source>
+        <translation>Taggar</translation>
+    </message>
+    <message>
+        <source>Favourite</source>
+        <translation>Favorit</translation>
+    </message>
+    <message>
+        <source>LowPriority</source>
+        <translation>Låg prioritet</translation>
+    </message>
+    <message>
+        <source>Members</source>
+        <translation>Medlemmar</translation>
+    </message>
+    <message>
+        <source>Joined members</source>
+        <translation>Anslutna medlemmar</translation>
+    </message>
+    <message>
+        <source>Invited members</source>
+        <translation>Inbjudna medlemmar</translation>
+    </message>
+    <message>
+        <source>Total members</source>
+        <translation>Medlemmar totalt</translation>
+    </message>
+    <message>
+        <source>Matrix room version</source>
+        <translation>Matrix rumsversion</translation>
+    </message>
+    <message>
+        <source>Is unstable</source>
+        <translation>Är instabil</translation>
+    </message>
+    <message>
+        <source>Extended</source>
+        <translation>Utökad</translation>
+    </message>
+</context>
+<context>
+    <name>RoomListPage</name>
+    <message>
+        <source>Rooms</source>
+        <translation>Rum</translation>
+    </message>
+</context>
+</TS>
